4e28f350d250048d04521fbc39c9dc9bd7738552
nithikornz <nithikorn999@gmail.com>
2025-09-17 18:47:26 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -3,7 +3,7 @@ j = 0
 while i <= 10:
     result = ""
     while j <= 10:
-        result = result + " " +str(i*j)
+        result = result + " " + str(i*j)
         j+=1
     j=0
     print(f"Table de {i}:{result}")
